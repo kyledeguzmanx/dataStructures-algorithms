d06b10c27c274889a71de44f5dc40f57c12dc51d
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-03-18 22:44:42 -0700

    Create NativeLinkedList.java

---
+++ Linked List/NativeLinkedList.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class NativeLinkedList{
+  public static void main(String[] args){
+    LinkedList<Integer> list = new LinkedList<String>();
+    
+    list.add(5);
+    list.add(45);
+    list.remove();
+    
+    System.out.println(list.size());
+  }
+}
+
+/*
+https://www.geeksforgeeks.org/linked-list-in-java/
+*/
