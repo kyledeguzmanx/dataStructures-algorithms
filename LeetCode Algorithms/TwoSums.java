8a954c573b0acf740038620361244b458cb4b919
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-03-21 23:39:01 -0700

    Update TwoSums.java

--- LeetCode Algorithms/TwoSums.java
+++ LeetCode Algorithms/TwoSums.java
@@ -1,9 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         ArrayList<Integer> returnedArr = new ArrayList<Integer>();
-        
-        for(int i-0; i<nums.length; i++){
-            int sum = target;
             
         }
     }
