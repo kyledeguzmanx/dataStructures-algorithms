ee74c76d1a98eae4fe3b663f3f2d24b245bdceed
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-03-21 23:20:32 -0700

    Create TwoSums.java

---
+++ LeetCode Algorithms/TwoSums.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        ArrayList<Integer> returnedArr = new ArrayList<Integer>();
+        
+        for(int i-0; i<nums.length; i++){
+            int sum = target;
+            
+        }
+    }
+}
