4668cbef0314b97474ef560f80061a69f56855c5
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-03-22 22:31:30 -0700

    Update TwoSums.java

--- LeetCode Algorithms/TwoSums.java
+++ LeetCode Algorithms/TwoSums.java
@@ -7,5 +7,13 @@ class Solution {
         for(int i=0; i<nums.length; i++){ //we use hashmap for indexOf function
             array.put(nums[i], i);
         }
+        
+         //find matchings
+        for(int i=0; i<nums.length; i++){
+            if(array.containsKey(target-nums[i]) && array.get(target-nums[i]) != i){
+                int[] returnArr = new int[]{i, array.get(target-nums[i])};
+                return returnArr;
+            }
+        }  
     }
 }
