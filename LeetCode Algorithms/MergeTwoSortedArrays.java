d8192e5de6bddcbcd36879e6df6fed169f605293
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-03-22 22:51:17 -0700

    Update MergeTwoSortedArrays.java

--- LeetCode Algorithms/MergeTwoSortedArrays.java
+++ LeetCode Algorithms/MergeTwoSortedArrays.java
@@ -8,3 +8,24 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+    
+        ListNode head = new ListNode();
+        ListNode one = list1;
+        ListNode two = list2;
+
+        //establish head;
+        if(one.val < two.val){
+            head = one.val;     
+        }
+        else{
+            head = two.val;
+        }
+        
+        while(one.next != null & two.next != null){
+            ListNode newNode = new ListNode();
+            
+        }
+    }
+}
