cf7638217d80c4d590b978cc8f4fc17df76e8878
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-03-22 22:30:52 -0700

    Update TwoSums.java

--- LeetCode Algorithms/TwoSums.java
+++ LeetCode Algorithms/TwoSums.java
@@ -3,7 +3,8 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> array = new HashMap<>();
         
-        for(int i : nums){
+        //convert array to hashmap
+        for(int i=0; i<nums.length; i++){ //we use hashmap for indexOf function
             array.put(nums[i], i);
         }
         
